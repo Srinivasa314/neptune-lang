0236cbe19838659c027b592e4cb0945c11df341d
Srikanth <srikanth.mahesh1234@gmail.com>
2021-09-18 12:27:13 +0530

    some fixes

--- build-clang.py
+++ build-clang.py
@@ -3,11 +3,16 @@ import subprocess
 import sys
 import shutil
 import os
+import platform
 
 if shutil.which('clang++') is None:
     sys.exit(f'clang++ not found')
 os.environ['CXX'] = 'clang++'
-os.environ['AR'] = 'llvm-lib'
-os.environ['RUSTFLAGS'] = "-C linker=lld-link"
+if platform.system()=='Windows':
+    os.environ['AR'] = 'llvm-lib'
+if platform.system()=='Windows':
+    os.environ['RUSTFLAGS'] = '-Clinker=lld-link'
+elif platform.system()=='Linux':
+    os.environ['RUSTFLAGS']= '-Clink-arg=-fuse-ld=lld'
 os.environ['CXXFLAGS'] = '-flto=thin'
 subprocess.run('cargo build --release', shell=True)
--- test.lua
+++
@@ -1,8 +0,0 @@
-function f()
-    local i=0
-    while i<100000000 do
-        i=i+1
-    end
-end
-
-f()
