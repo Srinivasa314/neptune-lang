4b9ebc3164eee73e288b844a5b9ccc826692de7d
Srikanth <srikanth.mahesh1234@gmail.com>
2021-10-15 20:13:07 +0530

    add more benchmarks

---
+++ bench/btree.py
@@ -0,0 +1,41 @@
+
+# contributed by Antoine Pitrou
+# modified by Dominique Wahli
+# modified by Heinrich Acker
+# 2to3
+# *reset*
+
+import sys
+
+def make_tree(depth):
+    if not depth: return [None, None]
+    depth -= 1
+    return [make_tree(depth), make_tree(depth)]
+
+def check_tree(node):
+    [left, right] = node
+    if not left: return 1
+    return 1 + check_tree(left) + check_tree(right)
+
+min_depth = 4
+max_depth = 18
+stretch_depth = max_depth + 1
+
+print("stretch tree of depth %d\t check:" % 
+    stretch_depth, check_tree(make_tree(stretch_depth)))
+
+long_lived_tree = make_tree(max_depth)
+
+iterations = 2**max_depth
+
+for depth in range(min_depth, stretch_depth, 2):
+
+    check = 0
+    for i in range(0, iterations):
+        check += check_tree(make_tree(depth))
+
+    print("%d\t trees of depth %d\t check:" % (iterations, depth), check)
+    iterations //= 4
+
+print("long lived tree of depth %d\t check:" % 
+    max_depth, check_tree(long_lived_tree))
---
+++ bench/btree.rb
@@ -0,0 +1,51 @@
+# The Computer Language Benchmarks Game
+# https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+#
+# contributed by Jesse Millikan
+# Modified by Wesley Moxam
+# Modified by Scott Leggett
+# *reset*
+
+def item_check(left, right)
+    if left
+        1 + item_check(*left) + item_check(*right)
+    else
+        1
+    end
+end
+
+def bottom_up_tree(depth)
+    if depth > 0
+        depth -= 1
+        [bottom_up_tree(depth), bottom_up_tree(depth)]
+    else
+        [nil, nil]
+    end
+end
+
+max_depth = 18
+min_depth = 4
+
+max_depth = [min_depth + 2, max_depth].max
+
+stretch_depth = max_depth + 1
+stretch_tree = bottom_up_tree(stretch_depth)
+
+puts "stretch tree of depth #{stretch_depth}\t check: #{item_check(*stretch_tree)}"
+stretch_tree = nil
+
+long_lived_tree = bottom_up_tree(max_depth)
+
+min_depth.step(max_depth, 2) do |depth|
+  iterations = 2**(max_depth - depth + min_depth)
+
+  check = 0
+
+  (1..iterations).each do |i|
+    check += item_check(*bottom_up_tree(depth))
+  end
+
+  puts "#{iterations}\t trees of depth #{depth}\t check: #{check}"
+end
+
+puts "long lived tree of depth #{max_depth}\t check: #{item_check(*long_lived_tree)}"
