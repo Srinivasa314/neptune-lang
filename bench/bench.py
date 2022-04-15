3348d09110122123e0295f0e899c84552637bb39
Srinivasa Mahesh <m.srinivasa1234@gmail.com>
2022-04-15 10:29:21 +0530

    include LuaJIT,geometric mean

--- bench/bench.py
+++ bench/bench.py
@@ -7,17 +7,27 @@ benchmarks = ['ack', 'btree', 'collatz', 'fib', 'nbody',
 
 languages = {'Neptune': ['../neptune-cli/target/release/neptune-cli', '.np'],
              'NodeJS(JIT off)': ['node', '-jitless', '.js'],
-             'Ruby': ['ruby', '.rb'],  'Lua': ['lua', '.lua'], 'Node(JIT)': ['node', '.js'], }
+             'Ruby': ['ruby', '.rb'],  'Lua': ['lua', '.lua'], 'NodeJS(JIT)': ['node', '.js'], 'LuaJIT(JIT off)': ['luajit', '-joff', '.lua'], 'LuaJIT(JIT)': ['luajit', '.lua']}
 
 results = {}
 for benchmark in benchmarks:
     results[benchmark] = {}
 
+results['Geometric Mean'] = {}
+for lang in languages:
+    results['Geometric Mean'][lang] = 1.0
+
 for benchmark in benchmarks:
     for lang in languages:
         results[benchmark][lang] = float(subprocess.run(languages[lang][:-1]+[benchmark + languages[lang][-1]],
                                                         stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.decode('utf-8').split('\n')[-2])
+        results['Geometric Mean'][lang] *= results[benchmark][lang]
 
+for lang in languages:
+    results['Geometric Mean'][lang] **= (1/len(benchmarks))
+    print(lang, results['Geometric Mean'][lang])
+
+benchmarks.append('Geometric Mean')
 for benchmark in benchmarks:
     fig, ax = plt.subplots()
     langs = list(languages.keys())
